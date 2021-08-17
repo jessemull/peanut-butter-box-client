1bd3122922481a92b62c2a54bf1cd7eea6ae8fff
Jesse Mull <jessemull@gmail.com>
2021-08-17 13:17:23 -0700

    Navigate to profile if already logged in

--- components/appbar/Icons.tsx
+++ components/appbar/Icons.tsx
@@ -1,15 +1,17 @@
 import Link from 'next/link'
+import { useContext } from 'react'
+import { OAuthContext } from '../../providers/oauth'
 import CartIcon from '../icons/Cart'
 import ProfileIcon from '../icons/Profile'
 import SearchIcon from '../icons/Search'
 import styles from './Icons.module.css'
 
-const icons = [{
+const icons = (isSignedIn: boolean) => [{
   Icon: SearchIcon,
   label: 'search'
 }, {
   Icon: ProfileIcon,
-  href: '/signin',
+  href: isSignedIn ? '/profile' : '/signin',
   label: 'profile'
 }, {
   Icon: CartIcon,
@@ -17,24 +19,28 @@ const icons = [{
   label: 'cart'
 }]
 
-const Icons = (): JSX.Element => (
-  <>
-    {icons.map(({ Icon, href, label }) =>
-      href
-        ? (
-            <div className={styles.icon_container} key={label}>
-              <Link href={href}>
-                <a><Icon /></a>
-              </Link>
-            </div>
-          )
-        : (
-            <div className={styles.icon_container} key={label}>
-              <Icon />
-            </div>
-          )
-    )}
-  </>
-)
+const Icons = (): JSX.Element => {
+  const { getIdToken } = useContext(OAuthContext)
+  const isSignedIn = Boolean(getIdToken())
+  return (
+    <>
+      {icons(isSignedIn).map(({ Icon, href, label }) =>
+        href
+          ? (
+              <div className={styles.icon_container} key={label}>
+                <Link href={href}>
+                  <a><Icon /></a>
+                </Link>
+              </div>
+            )
+          : (
+              <div className={styles.icon_container} key={label}>
+                <Icon />
+              </div>
+            )
+      )}
+    </>
+  )
+}
 
 export default Icons
--- components/bottom-nav/BottomNav.tsx
+++ components/bottom-nav/BottomNav.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
+import { useContext } from 'react'
+import { OAuthContext } from '../../providers/oauth'
 import CartIcon from '../icons/Cart'
 import ProfileIcon from '../icons/Profile'
 import styles from './BottomNav.module.css'
 
-const icons = [{
+const icons = (isSignedIn: boolean) => [{
   Icon: ProfileIcon,
-  href: '/profile',
+  href: isSignedIn ? '/profile' : '/signin',
   label: 'Profile'
 }, {
   Icon: CartIcon,
@@ -13,19 +15,23 @@ const icons = [{
   label: 'Cart'
 }]
 
-const Icons = (): JSX.Element => (
-  <div className={styles.bottom_nav}>
-    {icons.map(({ Icon, href, label }) => (
-      <Link href={href} key={label} passHref>
-        <div className={styles.icon_container_bottom_nav}>
-          <div className={styles.icon_container_bottom_nav_inner}>
-            <Icon size={26} />
-            <p className={styles.icon_container_bottom_nav_label}>{label}</p>
+const Icons = (): JSX.Element => {
+  const { getIdToken } = useContext(OAuthContext)
+  const isSignedIn = Boolean(getIdToken())
+  return (
+    <div className={styles.bottom_nav}>
+      {icons(isSignedIn).map(({ Icon, href, label }) => (
+        <Link href={href} key={label} passHref>
+          <div className={styles.icon_container_bottom_nav}>
+            <div className={styles.icon_container_bottom_nav_inner}>
+              <Icon size={26} />
+              <p className={styles.icon_container_bottom_nav_label}>{label}</p>
+            </div>
           </div>
-        </div>
-      </Link>
-    ))}
-  </div>
-)
+        </Link>
+      ))}
+    </div>
+  )
+}
 
 export default Icons
