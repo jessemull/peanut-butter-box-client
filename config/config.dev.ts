9b2cad54f7530884a5e63570addcd4e407bd6552
Jesse Mull <jessemull@gmail.com>
2021-08-18 11:27:21 -0700

    Point back to actual https site for okta

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -2,7 +2,7 @@ const baseUrl = 'https://zesve9r89j.execute-api.us-east-1.amazonaws.com/dev'
 
 const config = {
   healthcheckUrl: `${baseUrl}/healthcheck`,
-  redirectUri: 'http://dev.peanutbutterbox.org.s3-website-us-east-1.amazonaws.com',
+  redirectUri: 'https://dev.peanutbutterbox.org',
   usersUrl: `${baseUrl}/users`
 }
 
