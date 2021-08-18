737e6d00199aebe16a6ecea0acabade3b834de7f
Jesse Mull <jessemull@gmail.com>
2021-08-18 11:07:17 -0700

    Try s3 bucket redirect

--- config/config.dev.ts
+++ config/config.dev.ts
@@ -2,7 +2,7 @@ const baseUrl = 'https://zesve9r89j.execute-api.us-east-1.amazonaws.com/dev'
 
 const config = {
   healthcheckUrl: `${baseUrl}/healthcheck`,
-  redirectUri: 'https://dev.peanutbutterbox.org',
+  redirectUri: 'http://dev.peanutbutterbox.org.s3-website-us-east-1.amazonaws.com',
   usersUrl: `${baseUrl}/users`
 }
 
