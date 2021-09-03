a6ae056642415405971265dae5fde3d5a0cb9595
Jesse Mull <jessemull@gmail.com>
2021-09-02 19:32:13 -0700

    Added number of products to mobile bottom nav cart icon

--- components/bottom-nav/BottomNav.tsx
+++ components/bottom-nav/BottomNav.tsx
@@ -1,35 +1,33 @@
 import Link from 'next/link'
 import { useContext } from 'react'
+import { CartContext } from '../../providers/cart'
 import { OAuthContext } from '../../providers/oauth'
 import CartIcon from '../icons/Cart'
 import ProfileIcon from '../icons/Profile'
 import styles from './BottomNav.module.css'
 
-const icons = (isSignedIn: boolean) => [{
-  Icon: ProfileIcon,
-  href: isSignedIn ? '/profile' : '/signin',
-  label: 'Profile'
-}, {
-  Icon: CartIcon,
-  href: '/cart',
-  label: 'Cart'
-}]
-
 const Icons = (): JSX.Element => {
+  const { products } = useContext(CartContext)
   const { getIdToken } = useContext(OAuthContext)
   const isSignedIn = Boolean(getIdToken())
   return (
     <div className={styles.bottom_nav}>
-      {icons(isSignedIn).map(({ Icon, href, label }) => (
-        <Link href={href} key={label}>
-          <a className={styles.icon_container_bottom_nav}>
-            <div className={styles.icon_container_bottom_nav_inner}>
-              <Icon size={26} />
-              <p className={styles.icon_container_bottom_nav_label}>{label}</p>
-            </div>
-          </a>
-        </Link>
-      ))}
+      <Link href={isSignedIn ? '/profile' : '/signin'}>
+        <a className={styles.icon_container_bottom_nav}>
+          <div className={styles.icon_container_bottom_nav_inner}>
+            <ProfileIcon size={26} />
+            <p className={styles.icon_container_bottom_nav_label}>Profile</p>
+          </div>
+        </a>
+      </Link>
+      <Link href="/cart">
+        <a className={styles.icon_container_bottom_nav}>
+          <div className={styles.icon_container_bottom_nav_inner}>
+            <CartIcon numProducts={products.length} size={26} />
+            <p className={styles.icon_container_bottom_nav_label}>Cart</p>
+          </div>
+        </a>
+      </Link>
     </div>
   )
 }
