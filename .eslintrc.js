50bddd495f9a9bcc9124784bc8fc4235349d99ea
Jesse Mull <jessemull@gmail.com>
2021-09-14 20:58:39 -0700

    No exhaustive deps for hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
   ],
   rules: {
     '@next/next/no-img-element': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-hooks/exhaustive-deps': 'off'
   }
 }
